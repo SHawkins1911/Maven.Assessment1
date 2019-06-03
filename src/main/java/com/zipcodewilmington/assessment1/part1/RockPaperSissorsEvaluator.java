c5999802e6369d793fb5531ca7abbbdaf561d900
Stefun Hawkins <stefunhawkins@zipcoders-MacBook-Pro-8.local>
2019-06-03 10:57:50 -0400

    part 1 finished

--- src/main/java/com/zipcodewilmington/assessment1/part1/RockPaperSissorsEvaluator.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/RockPaperSissorsEvaluator.java
@@ -13,7 +13,22 @@ public class RockPaperSissorsEvaluator {
      * @return the respective winning move
      */
     public String getWinningMove(String handSign) {
-        return null;
+        String winSign = "";
+        switch (handSign){
+            case "scissor" : winSign = ("rock");
+                break;
+
+            case "rock" : winSign = ("paper");
+                break;
+
+            case "paper" : winSign = ("scissor");
+                break;
+
+            default: winSign = ("Invalid input");
+                break;
+        }
+
+        return winSign;
     }
 
     /**
@@ -21,7 +36,22 @@ public class RockPaperSissorsEvaluator {
      * @return the respective losing move
      */
     public String getLosingMove(String handSign) {
-        return null;
+        String loseSign = "";
+        switch (handSign){
+            case "scissor" : loseSign = ("paper");
+                break;
+
+            case "rock" : loseSign = ("scissor");
+                break;
+
+            case "paper" : loseSign = ("rock");
+                break;
+
+            default: loseSign = ("Invalid input");
+                break;
+        }
+
+        return loseSign;
     }
 
     /**
@@ -30,6 +60,14 @@ public class RockPaperSissorsEvaluator {
      * @return a string representative of the winning hand sign between the two players
      */
     public String getWinner(String handSignOfPlayer1, String handSignOfPlayer2) {
+        if (handSignOfPlayer1.equals(handSignOfPlayer2)) {
+            return "tie";
+        }else{
+            if (handSignOfPlayer1.equals("rock") && handSignOfPlayer2.equals("scissor")){
+                return "rock";
+            }
+        }
+
         return null;
     }
 }
